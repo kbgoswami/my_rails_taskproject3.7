8797dfcd18cb6bf5db5a7d62641f851606cdf943
Kulbhushan Goswami <kbgoswami196522@gmail.com>
2016-04-21 15:25:56 -0700

    added color field to products, insert $ sign for price and a little style

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419012927) do
+ActiveRecord::Schema.define(version: 20160421025815) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160419012927) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.integer  "price"
   end
 
 end
