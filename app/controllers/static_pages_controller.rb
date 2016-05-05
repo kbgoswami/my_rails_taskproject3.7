4a6c11c51cce83b1b04eea26494b928df2d701e0
Kulbhushan Goswami <kbgoswami196522@gmail.com>
2016-05-04 18:11:31 -0700

    Product-limit4 in Landing page and Partials for Products

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,8 @@ class StaticPagesController < ApplicationController
   def landing_page
     @featured_product = Product.first
   end
+
+  def landing_page
+  	@products = Product.limit(4)
+  end
 end
