fbf128b4b7b81d8ff249c9c7ce99be3a7bc72caf
Kulbhushan Goswami <kbgoswami196522@gmail.com>
2016-05-19 14:18:36 -0700

    if-else added for prod and dev environments

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,10 @@ class ProductsController < ApplicationController
   def index
     if params[:q]
     search_term = params[:q]
-    @products = Product.where("name LIKE ?", "%#{search_term}%")
-    # return our filtered list here
+    if Rails.env.development?
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      @products = Product.where("name ilike ?", "%#{search_term}%")
   else
     @products = Product.all
   end
