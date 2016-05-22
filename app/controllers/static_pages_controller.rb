cb40a44412c45ea40d0607bf0771210470cfd0e5
Kulbhushan Goswami <kbgoswami196522@gmail.com>
2016-05-22 10:30:00 -0700

    mailer added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,24 @@
 class StaticPagesController < ApplicationController
   def index
   end
+  
   def landing_page
     @featured_product = Product.first
   end
 
   def landing_page
-  @products = Product.limit(4)
-end
+  	@products = Product.limit(4)
+  end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'kbgoswami@comcast.net',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+
+  end
 
 end
