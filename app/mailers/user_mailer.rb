1da4f152aa517da641604cf7b1eb2509510e3940
Kulbhushan Goswami <kbgoswami196522@gmail.com>
2016-06-08 16:04:40 -0700

    user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
         :to => 'kbgoswami@comcast.net',
         :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  	@appname = "Bikesonsale"
+  	mail( :to => user.email,
+    :subject => "Welcome to #{@appname}!")
+  end
+
 end
